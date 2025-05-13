695f813bdd72d3d8b6dd83df8a32508c1fc50339
LordOfTheNugget <ncaccamo@live.ca>
2025-05-13 12:16:03 -0400

    stop emission when collecting

--- Assets/Collectable.cs
+++ Assets/Collectable.cs
@@ -6,6 +6,7 @@ public class Collectable : MonoBehaviour
     CharacterObject player;
     public float amount = 5f;
     bool isCollected = false;
+    public ParticleSystem particleSystem;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -25,6 +26,7 @@ public class Collectable : MonoBehaviour
             isCollected = true;
             player = other.GetComponent<CharacterObject>();
             player.Collect(amount);
+            particleSystem.enableEmission = false;
             Destroy(transform.parent.gameObject, 0.5f);
         }
     }
